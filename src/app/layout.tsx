5e94252decdaa28641d05f93191428508bae05f0
man <chan.man.tsz@tha.de>
2025-08-04 14:24:02 +0200

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,26 +26,4 @@ export const metadata: Metadata = {
 }
 
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-}
 
-export const themeColor = "#10B981"
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </head>
-      <body className="antialiased min-h-screen">{children}</body>
-    </html>
-  )
-}
